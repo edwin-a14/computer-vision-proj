27162dd7535401aba7ae674d6794b1cf67fc0d94
Yakov Shulepov <yakov.shulepov@gmail.com>
2025-10-27 13:08:22 -0400

    Detect red color regions and blobs

--- src/detect_color_shape.py
+++ src/detect_color_shape.py
@@ -11,7 +11,74 @@ def main():
         path = "data/raw/kaggle_roadsign/images/road"+str(i)+".png"
         #img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
         
-        img = cv2.imread(path)
+        orig_img = cv2.imread(path)
+        img = orig_img.copy()
+        hsv_image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        max_saturation = np.max(hsv_image[:,:,1])
+        color_thresh = 0.8*max_saturation
+        white_thresh = 0.2*max_saturation
+        #hsv_image[(hsv_image[:,:,1]<color_thresh) & (hsv_image[:,:,1]>white_thresh)]=0
+        hsv_image[hsv_image[:,:,1]<color_thresh]=0
+        # Limit to red objects
+        hsv_image[(hsv_image[:,:,0]>63) & (hsv_image[:,:,0]<200)] = 0
+
+        img = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
+        img = cv2.GaussianBlur(img, (75,75),5)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img[img>0]=255
+
+        # Find contours
+        contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Loop through contours and draw bounding boxes
+        for contour in contours:
+            # Get the bounding rectangle for the contour
+            x, y, w, h = cv2.boundingRect(contour)
+    
+            # Draw the rectangle on the original image
+            cv2.rectangle(orig_img, (x, y), (x + w, y + h), (0, 255, 0), 2) # Green color, 2 pixels thick
+
+        #cv2.imwrite("data/processed/colors/red/road"+str(i)+"_r.png",orig_img)
+        #continue
+
+
+        img = (orig_img[:,:,0]).copy()	
+        # Setup SimpleBlobDetector parameters
+        params = cv2.SimpleBlobDetector_Params()
+        
+        # Thresholds for binarization
+        params.minThreshold = 10
+        params.maxThreshold = 200
+        
+        # Filter by Area
+        params.filterByArea = True
+        params.minArea = 75 # ~ pi*5^2
+        
+        # Filter by Circularity
+        params.filterByCircularity = True
+        params.minCircularity = 0.1
+        
+        # Filter by Convexity
+        params.filterByConvexity = True
+        #params.minConvexity = 0.87
+        
+        # Filter by Inertia
+        params.filterByInertia = True
+        params.minInertiaRatio = 0.01
+        
+        # Create a detector with the parameters
+        detector = cv2.SimpleBlobDetector_create(params)
+        
+        # Detect blobs
+        keypoints = detector.detect(img)
+        
+        # Draw blobs as green circles
+        output = cv2.drawKeypoints(orig_img, keypoints, np.array([]), (255, 0, 0),
+                                cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+        cv2.imwrite("data/processed/colors/red/road"+str(i)+"_r.png",output)
+        continue
+
         r = img[:,:,2]
         g = img[:,:,1]
         b = img[:,:,0]
